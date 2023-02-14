5b6c80095a6102393137fc737b70be2a9a922206
adityaparab04 <adityaparab04@gmail.com>
2023-02-14 13:25:49 -0500

    floating navbar component added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 import './nav.css';
+import {FaHome} from 'react-icons/fa';
+import {SiAboutdotme} from 'react-icons/si';
+import {BiBookContent} from 'react-icons/bi';
+import {GrProjects} from 'react-icons/gr';
+import {MdContactMail} from 'react-icons/md';
+import { useState } from 'react';
 
 const Nav = () => {
+
+  const [active, setActive] = useState('#');
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' onClick={() => setActive('#')} className={active === '#' ? 'active' : ''}><FaHome/></a>
+      <a href='#about' onClick={() => setActive('#about')} className={active === '#about' ? 'active' : ''}><SiAboutdotme/></a>
+      <a href='#experience' onClick={() => setActive('#experience')} className={active === '#experience' ? 'active' : ''}><BiBookContent/></a>
+      <a href='#projects' onClick={() => setActive('#projects')} className={active === '#projects' ? 'active' : ''}><GrProjects/></a>
+      <a href='#contact' onClick={() => setActive('#contact')} className={active === '#contact' ? 'active' : ''}><MdContactMail/></a>
+    </nav>
   )
 }
 
